9b42cfcd13b100acb2b04f1454f2b9a7edbea05c
Brady Farley <bfarley822@gmail.com>
2022-07-08 21:13:55 -0600

    Changes styling for nav bar items, includes website title and logo

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,12 +1,28 @@
 <template>
   <header>
-    <div class="w-full grid justify-items-end p-5 bg-gray-700">
-        <div class="grid grid-cols-2 gap-x-4 text-white text-2xl">
+    <div class="w-full flex justify-between p-5 bg-gray-700">
+        <router-link to="/">
+            <div class="flex items-center text-white text-4xl">
+                <img src="@/assets/handshake.png" class="w-16 pr-4">
+                Contrex
+            </div>
+        </router-link>
+        <div class="grid grid-cols-2 gap-x-4 text-white flex items-center text-2xl">
             <router-link to="/">
-                <div class="py-1 px-4 hover:bg-white hover:text-gray-700 rounded-md">Home</div>
+                <div :class="['py-1 px-4 rounded-md', 
+                             selectedTab === 'home' ? 'bg-white text-gray-700' : 'hover:bg-gray-500 hover:text-gray-400']" 
+                     @click="updateSelection('home')"
+                >
+                     Home
+                </div>
             </router-link>
             <router-link to="about">
-                <div class="py-1 px-4 hover:bg-white hover:text-gray-700 rounded-md">About</div>
+                <div :class="['py-1 px-4 rounded-md', 
+                             selectedTab === 'about' ? 'bg-white text-gray-700' : 'hover:bg-gray-500 hover:text-gray-400']"
+                     @click="updateSelection('about')"
+                >
+                     About
+                </div>
             </router-link>
         </div>
     </div>
@@ -15,6 +31,19 @@
 
 <script>
 export default {
-  name: "NavBar",
+    name: "NavBar",
+    data: function() {
+        return {
+            selectedTab: 'home'
+        }
+    },
+    methods: {
+        updateSelection: function(selection) {
+            this.selectedTab = selection;
+        }
+    },
+    computed: {
+        
+    }
 };
 </script>
